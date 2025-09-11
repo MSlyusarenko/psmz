b6cdc0fb6b686510004d1f66072edd7cee9ffc13
MATVEY <75066032+MATVEY-Git-Hub@users.noreply.github.com>
2025-09-11 22:16:02 +0300

    Fix docker

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,7 @@ import { resolve } from 'path';
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-
-  vite: {
-    ssr: {
-      noExternal: ['@headlessui/vue']
-    }
-  },
-
+  ssr: false,
 
   alias: {
     '~~': resolve(__dirname),
